ba3cd78361d4f1211334a997abb8b586766f7855
Pedro Araújo <127028443+pedroara22@users.noreply.github.com>
2025-06-11 19:47:03 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser');
 const cors = require('cors');
 const upload = require('./multer');
 const fs = require('fs');
